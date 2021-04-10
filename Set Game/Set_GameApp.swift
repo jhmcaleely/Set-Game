40e01c670a04e56e1c1690996ee409e77b9d95e8
John McAleely <john@mcaleely.com>
2021-04-10 18:05:05 +0100

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by John McAleely on 10/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
